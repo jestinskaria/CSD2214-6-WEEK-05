604573d5a54234ebe4875e4d99194119f8ef701e
jestin skaria <jestinskaria15@gmail.com>
2019-02-04 17:04:03 -0500

    final commit

--- C0745078/js/main.js
+++ C0745078/js/main.js
@@ -1,4 +1,5 @@
-﻿
+﻿function study() {
     var date = new Date();
-        document.body.innerHTML = "<h1>Today date is " + date + "</h1>";
-   
+    document.body.innerHTML = "<h1>Today date is " + date + "</h1>";
+
+}
